f2ab0fa024af41c47cda2371f97c512e5c2158ef
Thausifullah Sherif K <93920557+Thausifs@users.noreply.github.com>
2023-07-18 11:41:28 +0530

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -4,8 +4,17 @@ import userRouter from "./routes/user.router";
 import listingRouter from "./routes/listing.routes";
 import teamMemberRouter from "./routes/teamMember.routes";
 import foodmodulerouter from './routes/foodmodule.routes';
+import { otpStorage } from '../src/controller/otpStorage'; 
+
 
 function routes(app: Express) {
+  app.get("/", (req, res) => {
+    // Retrieve the stored OTP using the otpStorage module
+    const storedOtp: string = otpStorage.getOtp();
+  
+    // Print the stored OTP in the response
+    res.send(`Backend server is running! Last OTP sent: ${storedOtp}`);
+  });
   app.get("/healthCheck", (req: Request, res: Response) => {
     res.sendStatus(200);
   });
